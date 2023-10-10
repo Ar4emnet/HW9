e3fc51b508ec608dff63a008a6c291240b4f032b
Artem <artemden.kolesnik@gmail.com>
2023-10-10 21:18:49 +0300

    fix bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 try:
     with open('text.txt', 'r') as first_file:
-        with open('text.txt', 'w') as second_file:
+        with open('text1.txt', 'w') as second_file:
             for line in first_file:
                 words = line.split()
                 long_words = [word for word in words if len(word) >= 7]
